6be5439b2c1c2a8e2401e791d1a28d2b81c97dab
Muhammad Aasim Samar <m.aasimsamar7@gmail.com>
2022-12-26 19:57:10 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const headerElem = document.querySelector(".header");
+
+mobile_nav.addEventListener("click", () => {
+  headerElem.classList.toggle("active");
+});
